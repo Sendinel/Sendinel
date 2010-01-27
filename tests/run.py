d3c0109bfa9dbb439127f9160a8af1938879cbe9
Johan Uhle <johan>
2010-01-27 21:04:56 +0100

    trieing stuff with /tests/run.py

--- tests/run.py
+++ tests/run.py
@@ -6,10 +6,21 @@ except ImportError:
     print ('please install nose to run the tests')
     sys.exit(1)
 
+# try:
+#     import sendinel
+# except ImportError:
+#     print ('can not find sendinel')
+#     sys.exit(1)
+
 try:
+    # make sure the current source is first on sys.path
+    sys.path.insert(0, '..')
     import sendinel
 except ImportError:
-    print ('can not find sendinel')
+    print ('Cannot find Sendinel to test: %s' % sys.exc_info()[1])
     sys.exit(1)
+else:
+    print ('Sendinel test suite running (Python %s)...' %
+           (sys.version.split()[0]))
     
 nose.main()
