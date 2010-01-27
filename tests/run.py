354757b8fb2b9799e782b43175b7edbd24c949ee
Johan Uhle <johan>
2010-01-27 20:20:48 +0100

    Added /tests/run.py to make nose work

---
+++ tests/run.py
@@ -0,0 +1,13 @@
+try:
+    import nose
+except ImportError:
+    print ('please install nose to run the tests')
+    sys.exit(1)
+
+try:
+    import sendinel
+except ImportError:
+    print ('can not find sendinel')
+    sys.exit(1)
+    
+nose.main()
