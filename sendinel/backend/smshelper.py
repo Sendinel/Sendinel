f5804cb7b91587f5cec3a42fcdea1d923a71ad73
unknown <lea.voget@.hpi.uni-potsdam.de>
2010-02-05 17:29:32 +0100

    started sms templates

--- sendinel/backend/smshelper.py
+++ sendinel/backend/smshelper.py
@@ -1,18 +1,30 @@
 from math import floor
 
-def generate_appointment_sms(date, doctor, hospital, name):
-    sms_format = "Dear %s, please remember your appointment" + \
-                " at the %s at %s with doctor %s"
+def generate_sms(specific_content, text):
+    if len(specific_content) == 0:
+        sms = generate_message_sms(text)
+    elif len(specific_content) == 4:
+        sms = generate_appointment_sms(specific_content, text)
+    return sms
+
+def generate_message_sms(text):
+    return text[0:160]
     
-    sms_static_text = sms_format % (('',) * 4)
-    chars_left = 160 - len(date) - len(sms_static_text)
+    
+def generate_appointment_sms(specific_content,text):
+    sms_static_text = text % (('',) * 4)
+    chars_left = 160 - len(specific_content.get('date')) - len(text)
     
     max_length = int(floor(chars_left/3))
 
-    sms =  sms_format % (name, hospital, date, doctor)
+    sms =  text % (specific_content.get('name'), specific_content.get('hospital'), \
+                    specific_content.get('date'),specific_content.get('doctor'))
   
     if len(sms) > 160:
-        sms = sms_format % (name[0:max_length], hospital[0:max_length], date, doctor[0:max_length])
+        sms = text % (specific_content.get('name')[0:max_length],\
+                        specific_content.get('hospital')[0:max_length], \
+                        specific_content.get('date'),specific_content.get('doctor')[0:max_length])
+                
     
     return sms
 
