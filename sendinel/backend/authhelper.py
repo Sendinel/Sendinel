ff2b984ae5f1704b247570675ddf703d42e2e4b0
Daniel Moritz <daniel.moritz@student.hpi.uni-potsdam.de>
2010-02-10 15:55:19 +0100

    write further tests and prepare log parsing for authentication

--- sendinel/backend/authhelper.py
+++ sendinel/backend/authhelper.py
@@ -1,27 +1,50 @@
 import re
 
-def authenticate(number):
-    try:
-        number = format_number(number)
-        return True
-    except ValueError:
-        return False
+from sendinel.backend.helper import NotObservedNumberException
 
+class AuthHelper:
+    """
+    Provide functionality needed for authenticating phone numbers
+    """
+    to_check = []
+    log_path = "C:/temp/call_log.txt"
+    
+    def clean_up_to_check(self):
+        self.to_check = []
 
-def format_number(number):
+    def authenticate(self, number):
+        try:
+            number = format_phone_number(number)
+            return True
+        except ValueError:
+            return False
+
+    def observe_number(self, number):
+        if self.to_check.count(number) < 1:
+            self.to_check.append(number)
+    
+    def check_log(self, number):
+        parse_log(self.log_path)
+    
+        if number == "0123456":
+            return True
+        else:
+            if number == "01234":
+                raise NotObservedNumberException('Number was not observed')
+            return False
+    
+def parse_log(log_file):
+    pass
+    
+def format_phone_number(number):
+    """
+    return the number as Integer or raise ValueError
+    """
     regex = re.compile('(\/|\+|-| )')
     new_number = regex.sub('', number)
-    try:
-        if is_number(new_number):
-            return new_number
-        else:
-            raise NoNumberError
-    except ValueError:
-        pass
-        raise ValueError
-        
-        
-def is_number(s):
-    for f in [int, ]:
-        return f(s)
+    if int(new_number) and new_number[0] == '0':
+        return new_number
+    else:
+        raise ValueError('please give national number without country prefix')    
+
     
--- sendinel/backend/tests/auth_test.py
+++ sendinel/backend/tests/auth_test.py
@@ -2,32 +2,58 @@ import unittest
 from sendinel.backend.authhelper import *
 
 class AuthTest(unittest.TestCase):
+    def setUp(self):
+        self.ah = AuthHelper()
+
     def test_number_formating(self):
+        number = "0123456789"
+        self.assertEquals(format_phone_number(number), "0123456789")             
         number = "+0123456789"
-        self.assertEquals(format_number(number), "0123456789")
+        self.assertEquals(format_phone_number(number), "0123456789")
         number = "+01234/56789"
-        self.assertEquals(format_number(number), "0123456789")
+        self.assertEquals(format_phone_number(number), "0123456789")
         number = "+01234 56789"
-        self.assertEquals(format_number(number), "0123456789")
+        self.assertEquals(format_phone_number(number), "0123456789")
         number = "+0 1234 56789"
-        self.assertEquals(format_number(number), "0123456789")        
+        self.assertEquals(format_phone_number(number), "0123456789")        
         number = "+01234-56789"
-        self.assertEquals(format_number(number), "0123456789")
+        self.assertEquals(format_phone_number(number), "0123456789")
         number = "abc"
-        self.assertRaises(ValueError, format_number, number)
+        self.assertRaises(ValueError, format_phone_number, number)
         number = "0123a45678"
-        self.assertRaises(ValueError, format_number, number)        
-        
-    def test_is_number(self):
-        number = "123"
-        self.assertTrue(is_number(number))
-        number = "abc"
-        self.assertRaises(ValueError, is_number, number)
-        number = "12 3"
-        self.assertRaises(ValueError, is_number, number)
+        self.assertRaises(ValueError, format_phone_number, number)
+        number = "+49123456789"
+        self.assertRaises(ValueError, format_phone_number, number)          
 
     def test_authenticate(self):
         number = "01621785295"
-        self.assertTrue(authenticate(number))
+        self.assertTrue(self.ah.authenticate(number))
         number = "noValidNumber"
-        self.assertFalse(authenticate(number))
+        self.assertFalse(self.ah.authenticate(number))
+        
+    def test_check_log(self):
+        self.ah.observe_number("0123456")
+        self.ah.observe_number("0678945")
+        
+        # generate fake log file with call from number 0123456
+        
+        self.assertTrue(self.ah.check_log("0123456"))
+        self.assertFalse(self.ah.check_log("0678945"))
+        
+        self.assertRaises(NotObservedNumberException, self.ah.check_log, "01234")
+        
+    def test_observe_number(self):
+        self.ah.clean_up_to_check()
+        self.assertEquals(len(self.ah.to_check), 0)
+        number = "0123456"
+        self.ah.observe_number(number)
+        self.ah.observe_number(number)
+        self.assertEquals(len(self.ah.to_check), 1)
+        
+        
+    def test_parse_log(self):
+        fake = open("fake_file_log", 'w')
+        fake.write("1265799669\t02/10/2010-12:01:09\t012345678\t2428534")
+        fake.close()
+        parse_log("fake_file_log")
+        
