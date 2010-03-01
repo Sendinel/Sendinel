60795f8cb6628b63db5039ebd7bc28fb092759a6
unknown <thomas.klingbeil@.hpi.uni-potsdam.de>
2010-03-01 14:24:57 +0100

    created method for creating the needed voice files including basic caching and integration

--- sendinel/backend/voicecall.py
+++ sendinel/backend/voicecall.py
@@ -7,6 +7,9 @@ try:
     import os
     import pwd
     import shutil
+    from subprocess import Popen, PIPE
+
+    from hashlib import md5
     
     linux_available = True
     
@@ -22,6 +25,18 @@ class Voicecall:
         self.asterisk_spool_dir = sendinel.settings.ASTERISK_SPOOL_DIR  
         self.asterisk_extension = sendinel.settings.ASTERISK_EXTENSION
         self.asterisk_sip_account = sendinel.settings.ASTERISK_SIP_ACCOUNT
+        self.asterisk_festivalcache = "/var/lib/asterisk/festivalcache"
+        
+    def create_voicefile(self, text):
+        hash = md5(text).hexdigest()
+        filename = "%s/%s.ulaw" % (self.asterisk_festivalcache, hash)
+        if not os.path.exists(filename):
+			args = "-o %s -otype ulaw -" % (filename)
+			Popen(["text2wave"] + args.split(" "),stdin=PIPE)
+            p.communicate(input=text)
+		else:
+            pass
+        return "%s/%s" % (self.asterisk_festivalcache, hash)
 
     def create_spool_content(self, number, voicefile, extension, sip_account, context):
         """
@@ -92,15 +107,15 @@ Set: PassedInfo=%s
         except:
             return False
             
-    def conduct_call(self, number, voicefile, context): 
+    def conduct_call(self, number, text, context): 
         """
             If Linux operating system is available, conduct a voice call
             
             @param  number:     Phone number to be called
             @type   number:     String
             
-            @param  voicefile:  name of the sound file to play when the call is conducted
-            @type   voicefile:  String
+            @param  text:  text to be played when the call is conducted
+            @type   text:  String
             
             @param  context:    Asterisk call context to be used for the outgoing call
             @type   context:    String
@@ -109,6 +124,7 @@ Set: PassedInfo=%s
         """
         
         if linux_available:
+            voicefile = create_voicefile(text)
             content = self.create_spool_content(number, voicefile, self.asterisk_extension, self.asterisk_sip_account, context)
             self.create_spool_file("tmp", content)
             return self.move_spool_file("tmp")
