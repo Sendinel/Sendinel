767686b7fb3a882629ed11306d96ae9bcdf2f1f3
Patrick Hennig <patrick.hennig@student.hpi.uni-potsdam.de>
2010-03-02 18:06:12 +0100

    merged voicecall output model

--- sendinel/backend/tests/output_test.py
+++ sendinel/backend/tests/output_test.py
@@ -43,3 +43,25 @@ class OutputTest(unittest.TestCase):
         sendinel.backend.output.bluetooth.send_vcal = send_vcal1
         sendinel.backend.output.send(d)
         sendinel.backend.output.bluetooth.send_vcal = self.send_vcal_old
+
+    def test_send_voiceoutputobject(self):
+        self.phone_number = "test"
+        self.data = "1234"
+        d = sendinel.backend.output.VoiceOutputData()
+        d.phone_number = self.phone_number
+        d.data = self.data
+        
+        
+        def conduct_call1(self, number, text, context):
+            OutputTest.numberResult = number
+            OutputTest.textResult = text
+            
+
+              
+        self.conduct_call_old = sendinel.backend.output.voicecall.Voicecall.conduct_call
+        
+        sendinel.backend.output.voicecall.Voicecall.conduct_call = conduct_call1
+        sendinel.backend.output.send(d)
+        self.assertEquals(OutputTest.numberResult,self.phone_number)
+        self.assertEquals(OutputTest.textResult,self.data)
+        sendinel.backend.output.voicecall.Voicecall.conduct_call = self.conduct_call_old
