35e17f765a7e6882373df81c3ce18ff0bfbd6c73
lea.voget <lea.voget@hpi>
2010-03-11 18:49:48 +0100

    you can see pictures, text and video do not work yet

--- sendinel/knowledgebase/urls.py
+++ sendinel/knowledgebase/urls.py
@@ -1,17 +1,8 @@
 from django.conf.urls.defaults import patterns, url
 
 urlpatterns = patterns("",
-    url(r"^$", 'sendinel.knowledgebase.views.index', name = 'knowledgebase_index')
-    # url(r"^list_infoservices/$", 'sendinel.staff.views.list_infoservices', 
-        # name = 'staff_list_infoservices'),
-    # url(r"^infoservice/create/$", 'sendinel.staff.views.create_infoservice', 
-        # name = 'staff_infoservice_create'),
-    # url(r"^infoservice/members/(?P<id>\d+)$", 
-        # 'sendinel.staff.views.list_members_of_infoservice', 
-        # name = 'staff_infoservice_members'),
-    # url(r"^infoservice/members/(?P<id>\d+)/(?P<patient_id>\d+)$", 'sendinel.staff.views.delete_members_of_infoservice', 
-        # name = 'staff_infoservice_members_delete'),
-    # url(r"^create_infomessage/(?P<id>\d+)/$", 
-        # 'sendinel.staff.views.create_infomessage', 
-        # name = 'staff_create_infomessage')
+    url(r"^$", 'sendinel.knowledgebase.views.index', name = 'knowledgebase_index'),
+    url(r"^show/(?P<file_id>\d+)/$", 'sendinel.knowledgebase.views.show', 
+         name = 'knowledgebase_show')
+    
     )
--- sendinel/knowledgebase/views.py
+++ sendinel/knowledgebase/views.py
@@ -1,14 +1,51 @@
-import os, glob
+import os
+from copy import copy
 
+from django.http import HttpResponse
 from django.shortcuts import render_to_response
-from sendinel.settings import KNOWLEDGEBASE_DIRECTORY
 from django.template import RequestContext
+
+from sendinel.settings import KNOWLEDGEBASE_DIRECTORY, \
+                              MEDIA_URL
+from sendinel.logger import logger
+
 
 
 def index(request):
-
     files = os.listdir(KNOWLEDGEBASE_DIRECTORY)
+    for file in copy(files):
+        if file.startswith('.'): files.remove(file)
+        
+    files = dict(zip(range(0,(len(files))), files))
+    request.session['numbered_files'] = files
     
     return render_to_response('knowledgebase/index.html',
                               locals(),
                               context_instance=RequestContext(request))
+
+def show(request, file_id):
+
+    file_name = request.session['numbered_files'][int(file_id)]
+    
+    
+    if file_name.endswith('.jpg') or file_name.endswith('.jpeg'):
+        path_name = MEDIA_URL + "knowledgebase/" + file_name
+        return render_to_response('knowledgebase/show_jpg.html',
+                              locals(),
+                              context_instance=RequestContext(request))
+    elif file_name.endswith('.flv'):
+        return render_to_response('knowledgebase/show_video.html',
+                              locals(),
+                              context_instance=RequestContext(request))
+    elif file_name.endswith('.txt'):
+        path_name = KNOWLEDGEBASE_DIRECTORY + '/' + file_name
+        
+        try:
+            text = open(path_name).read()
+        except:
+            logger.error('Could not open file' + file_name)
+        
+        return render_to_response('knowledgebase/show_txt.html',
+                              locals(),
+                              context_instance=RequestContext(request))    
+    
