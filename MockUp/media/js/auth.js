1ae62be2dc03a9014bd7100daf6233932e4aaa4a
Daniel Moritz <daniel.moritz@student.hpi.uni-potsdam.de>
2010-03-16 13:12:04 +0100

    created mockup files for input testing

---
+++ MockUp/media/js/auth.js
@@ -0,0 +1,39 @@
+var check_for_call = function() {
+    var url = $("#ajax_url").val();
+    $.post(url,
+        {
+            number: $("#number").val()
+        },
+        function(json) {
+   
+            if(json && json.status) {
+                var statusText = $("#auth_status");
+                
+                switch(json.status) {                
+                    case "waiting":                        
+                        statusText.text("Waiting for your call");
+                        window.setTimeout("check_for_call()",1000);
+                    break;
+                    
+                    case "received":                        
+                        statusText.text("Thank you! Your telephone number has been authenticated.");
+                        var next = $("#next").val();
+                        window.location.replace(next);
+                    break;
+                    
+                    case "failed":
+                        statusText.text("Sorry, the authentication of your telephone number failed. Please try again.");
+                        $("#auth_spinner").hide();
+                                                
+                    break;
+                }
+            }
+        },
+        "json");
+   
+        
+};
+
+$(document).ready(check_for_call);
+
+
