b69c32b583c414e6478fabd8ff59999e782007d1
Daniel Moritz <daniel.moritz@student.hpi.uni-potsdam.de>
2010-03-18 12:01:03 +0100

    added next button to phone authentication page  - is only displayed if the authentication was successful

--- sendinel/web/views.py
+++ sendinel/web/views.py
@@ -139,7 +139,7 @@ def authenticate_phonenumber(request):
             number = fill_authentication_session_variable(request)
             logger.info("Starting authentication with %s" % AUTH_NUMBER)
             auth_number = AUTH_NUMBER
-            next = request.GET.get('next','')
+            nexturl = reverse('web_index')
             return render_to_response('web/authenticate_phonenumber_call.html', 
                               locals(),
                               context_instance = RequestContext(request))
