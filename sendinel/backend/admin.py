2732dc60a6e5d7ad04490a239b5377d0db19252d
Michael Frister <mf@meeee.local>
2010-03-19 09:16:41 +0100

    Added infoservices to admin backend.

--- sendinel/backend/admin.py
+++ sendinel/backend/admin.py
@@ -3,6 +3,7 @@ from django.contrib import admin
 from sendinel.backend.models import Doctor, Patient, Hospital, \
                                     HospitalAppointment, \
                                     InfoMessage, \
+                                    InfoService, \
                                     ScheduledEvent, \
                                     AuthenticationCall, \
                                     Subscription
@@ -12,6 +13,7 @@ admin.site.register(Patient)
 admin.site.register(Hospital)
 admin.site.register(HospitalAppointment)
 admin.site.register(InfoMessage)
+admin.site.register(InfoService)
 admin.site.register(ScheduledEvent)
 admin.site.register(AuthenticationCall)
 admin.site.register(Subscription)
