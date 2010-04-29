ed7b30f6f018e61a3889f2e552e911b5541bb72f
Michael Frister <michael.frister+git@gmail.com>
2010-04-29 11:16:46 +0200

    Made scheduler run as daemon. Updated start/stop script. Added INSTALL file with dependencies for scheduler.

---
+++ sendinel/scheduler.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+# This script needs write permissions to the sendinel directory.
+
+sendinelPath="$(dirname $0)"
+pidFile="$sendinelPath/scheduler.pid"
+logFile="/tmp/sendinelScheduler.log"
+
+start() {
+    echo -n "Starting Sendinel scheduler"
+    cd $sendinelPath && \
+    python "$sendinelPath/backend/scheduler.py" $pidFile >> "$logFile" 2>&1
+    echo "."
+}
+
+stop() {
+    echo -n "Stopping sendinel scheduler"
+    if [ -f "$pidFile" ]; then
+        pid=$(cat $pidFile)
+        kill $pid
+        # pid file should be removed by python
+    else
+        echo "Warning: Scheduler not running - no PID file found."
+    fi
+
+    echo "."
+}
+
+case "$1" in
+ start)
+    start
+    ;;
+ stop)
+    stop
+    ;;
+ restart)
+    stop
+    start
+    ;;
+ *)
+    echo "Usage: $0 {start|stop|restart}" >&2
+    exit 1
+    ;;
+esac
+
+exit 0
+
+
