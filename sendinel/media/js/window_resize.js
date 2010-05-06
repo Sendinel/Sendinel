4e8c4d638d4ac2b25a90b86739d21e52ac3b97b8
Philipp Giese <Phil@Phils-MacBook.local>
2010-05-06 13:16:57 +0200

    refactored css to a more dynamic version part one

--- sendinel/media/js/window_resize.js
+++ sendinel/media/js/window_resize.js
@@ -1,27 +1,15 @@
 var div_resize = function() {
 
-    var heightContent = window.innerHeight - ($('#header').height() + $('#footer').height());
-    var heightNotification = heightContent-(parseInt($('#notifications').css('padding-top'))*2);
+    var heightContent = window.innerHeight - ($('#header').height() + $('#footer').height());    
+    $("#content").height(heightContent);
     
-    if($('#notifications').height()<heightContent){
-        $('#notifications').height(heightNotification);
+    if($("#notifications").height() > heightContent) {
+        var backgroundWidth = 10;
+        $('#content').css("background-position", ($("#content").width() / 2) + backgroundWidth + "px 0px");
+    } else {
+        $('#content').css("background-position", "50% 0%");
     }
-    $('#content').height(heightContent);
-    
-    //Setting the width of the main menu
-    var scrollbarOffset = 15;
-    var widthContent = window.innerWidth / 2;   
-    var notificationOffset = parseInt($('#notifications').css('padding-left'))+parseInt($('#notifications').css('padding-right'));
-    var groupsOffset =  parseInt($('#groups').css('padding-left')) +parseInt($('#groups').css('padding-right'));
-    var borderOffset = parseInt($('#notifications').css('borderRightWidth'));
-    
-    var widthNotification = widthContent - (scrollbarOffset + notificationOffset + borderOffset )
-    var widthGroups = widthContent - (scrollbarOffset + groupsOffset +borderOffset )
-    
-    $('#notifications').width(widthNotification);
-    $('#groups').width(widthContent-50);
 };
 
 $(window).resize(div_resize); 
-
 $(document).ready(div_resize);
