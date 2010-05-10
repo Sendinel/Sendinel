a00b3d9c34f4e491e46ddbfbea2718203c975af9
Thomas Klingbeil <thomasklingbeil@ubuntu.ubuntu-domain>
2010-05-10 18:40:28 +0200

    code quality improvements texthelper tests

--- sendinel/backend/tests/texthelper_test.py
+++ sendinel/backend/tests/texthelper_test.py
@@ -21,59 +21,69 @@ class TesthelperTest(unittest.TestCase):
         self.assertEquals(date["time"], "eleven forty")
         
     def test_generate_text_appointment(self):
-        text = texthelper.generate_text({'date': "13.2.98", 'time': "3:39", 'doctor': "ms daily-binnessy-dayteewart", \
+        text = texthelper.generate_text({'date': "13.2.98", 'time': "3:39", \
+                            'doctor': "ms daily-binnessy-dayteewart", \
                             'hospital': "hodpiel hospital at the " +\
                             "lake with the frog and the tree",\
                             'name': "mr jameson-bitterall-wertifial"},\
-                             Template("$name, please remember your appointment" + \
-                                " at the $hospital at $date, $time with doctor $doctor"))  
-        should_text = texthelper.SMS_SALUTATION + "mr jameson-bitterall-wertif, please remember " +\
-                        "your appointment at the hodpiel hospital at the " + \
-                        "lak at 13.2.98, 3:39 with doctor ms daily-binnessy-dayteewar"
+                             Template("$name, please remember your" + \
+                                " appointment at the $hospital at $date," + \
+                                " $time with doctor $doctor"))  
+        should_text = texthelper.SMS_SALUTATION + "mr jameson-bitterall-" + \
+                        "wertif, please remember your appointment at the " + \
+                        "hodpiel hospital at the lak at 13.2.98, 3:39 with " + \
+                        "doctor ms daily-binnessy-dayteewar"
         self.assertTrue(len(text) <= 160)
         self.assertEquals (text, should_text)
 
     def test_generate_text_message(self):
-        text = texthelper.generate_text({'free_text':"Mrs. Joirie, your medicine is there. " \
-                                + "Please remember to pay 2 $."}, Template("$free_text"))
-        should_text = texthelper.SMS_SALUTATION + "Mrs. Joirie, your medicine is there. Please remember to pay 2 $."
+        text = texthelper.generate_text({'free_text':"Mrs. Joirie, your " + \
+                        "medicine is there. " + \
+                        "Please remember to pay 2 $."}, Template("$free_text"))
+        should_text = texthelper.SMS_SALUTATION + "Mrs. Joirie, your " + \
+                        "medicine is there. Please remember to pay 2 $."
         self.assertEquals (text, should_text)
         self.assertTrue(len(text) <= 160)
         
     def test_generate_appointment_text_cut_off(self):
-        text =  texthelper.generate_text({'text': "Hello my name is james I am 22 years old " + \
-                             "and i really want to go to bali and make holidays " + \
-                             "since my last holidays are ages ago and i think a " + \
-                             "little bit less stress would be fun to have"}, \
-                             Template ('What an interesting text :$text'))
-        should_text = texthelper.SMS_SALUTATION + "What an interesting text :Hello my name is james I am 22 " + \
-                        "years old and i really want to go to bali and make " + \
+        text =  texthelper.generate_text({'text': "Hello my name is james I" + \
+                            " am 22 years old and i really want to go to " + \
+                            "bali and make holidays since my last holidays" + \
+                            " are ages ago and i think a " + \
+                            "little bit less stress would be fun to have"}, \
+                            Template ('What an interesting text :$text'))
+        should_text = texthelper.SMS_SALUTATION + "What an interesting text" + \
+                        " :Hello my name is james I am 22 years old and i " + \
+                        "really want to go to bali and make " + \
                         "holidays since my last holidays are ages ago a"
         self.assertTrue(len(text) <= 160)
         self.assertEquals(should_text, text)
 
     def test_generate_appointment_text_no_cut(self):
-        text =  texthelper.generate_text({'text': "Hello my name is james I am 22 years old " + \
-                             "and i really want to go to bali and make holidays " + \
-                             "since my last holidays are ages ago and i think a " + \
-                             "little bit less stress would be fun to have"}, \
-                             Template ('What an interesting text :$text'), False)
-        should_text = "What an interesting text :Hello my name is james I am 22 years old " + \
-                             "and i really want to go to bali and make holidays " + \
-                             "since my last holidays are ages ago and i think a " + \
-                             "little bit less stress would be fun to have"
+        text =  texthelper.generate_text({'text': "Hello my name is james I" + \
+                            " am 22 years old and i really want to go to " + \
+                            "bali and make holidays since my last holidays" + \
+                            " are ages ago and i think a " + \
+                            "little bit less stress would be fun to have"}, \
+                            Template ('What an interesting text :$text'), False)
+        should_text = "What an interesting text :Hello my name is james I " + \
+                            "am 22 years old and i really want to go to " + \
+                            "bali and make holidays since my last holidays " + \
+                            "are ages ago and i think a " + \
+                            "little bit less stress would be fun to have"
         self.assertTrue(len(text) >= 160)
         self.assertEquals(should_text, text)
         
     def test_generate_appointment_text_cut_off_special_fields(self):
-        specific_content = {'date':"13.2.98, 3:39" ,\
-                            'doctor':"abcdefghijklmnopqrstuvwxyzabcd",\
+        specific_content = {'date':"13.2.98, 3:39" , \
+                            'doctor':"abcdefghijklmnopqrstuvwxyzabcd", \
                             'hospital':"hodpielitzkicitziktidiiiiii hospital", \
                             'name':"mr joijjliputututututututututu"}
-        text_template = Template("Dear $name, please remember your appointment" + \
-            " at the $hospital at $date with doctor $doctor")   
+        text_template = Template("Dear $name, please remember your " + \
+            "appointment at the $hospital at $date with doctor $doctor")   
         text = texthelper.generate_text(specific_content, text_template)
-        should_text = texthelper.SMS_SALUTATION + "Dear mr joijjlipututututututut, please remember your " + \
+        should_text = texthelper.SMS_SALUTATION + "Dear mr " + \
+                        "joijjlipututututututut, please remember your " + \
                         "appointment at the hodpielitzkicitziktidiiii at " + \
                         "13.2.98, 3:39 with doctor abcdefghijklmnopqrstuvwxy"
         self.assertTrue(len(text) <= 160)
