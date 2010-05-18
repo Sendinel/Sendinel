f5cbad1133c338bf10589c67b4928596ab2c64d1
Daniel Moritz <daniel.moritz@student.hpi.uni-potsdam.de>
2010-05-18 11:39:05 +0200

    - centered all content forms - button label for the "new"-button is now dynamic - new picture for the new button

--- sendinel/web/utils.py
+++ sendinel/web/utils.py
@@ -1,17 +1,7 @@
 from datetime import datetime
 
-from django.shortcuts import render_to_response
-from django.template import RequestContext
-
 from sendinel.backend.authhelper import format_and_validate_phonenumber
 from sendinel.backend.models import WayOfCommunication
-
-def render_status(request, success, title, message, \
-                          backurl = None, nexturl = None):
-                          
-    return render_to_response('web/status_message.html', 
-                          locals(),
-                          context_instance = RequestContext(request))
                           
 def fill_authentication_session_variable(request):
     number = request.session["patient"].phone_number
