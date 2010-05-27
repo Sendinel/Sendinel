4135598498fe4b0929d6e1ce44f730be8891c070
Michael Frister <michael.frister+git@gmail.com>
2010-05-27 11:02:12 +0200

    Using ModelForm for InfoService validation now. This way unique errors are catched.

--- sendinel/infoservices/forms.py
+++ sendinel/infoservices/forms.py
@@ -1,13 +1,18 @@
-from django.forms import CharField, Form
+from django.forms import CharField, Form, ModelForm
 from django.utils.translation import ugettext as _
 
+from sendinel.infoservices.models import InfoService
 
 class InfoMessageValidationForm(Form):
     text = CharField(error_messages={ \
                         'required': _('Please enter a text to send'), \
                         'invalid': _('The text contains invalid characters')})
 
-class InfoserviceValidationForm(Form): 
+class InfoServiceValidationForm(ModelForm):
+    class Meta:
+        model = InfoService
+        exclude = ['members']
+
     name = CharField(error_messages={ \
                         'required': _('Please enter a name'), \
                         'invalid': _('The name contains invalid characters')})
